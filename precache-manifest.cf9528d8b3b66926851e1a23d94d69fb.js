7c768478f9ca0e560a0408a5202e0e2a4d1eca14
Aravind Kumar <aravindhalahalli.81@gmail.com>
2020-10-08 14:20:32 +0530

    Updates

---
+++ precache-manifest.cf9528d8b3b66926851e1a23d94d69fb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8cf4eba45c1434c2a6c5090b706d44",
+    "url": "/halahalliaravind/EduvanzaTask/index.html"
+  },
+  {
+    "revision": "835c5b7508f740b4ac29",
+    "url": "/halahalliaravind/EduvanzaTask/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "78aa0ddec7a31b0c650b",
+    "url": "/halahalliaravind/EduvanzaTask/static/js/2.277985d9.chunk.js"
+  },
+  {
+    "revision": "68ec34cacd0b6dc1582fb5b44a013363",
+    "url": "/halahalliaravind/EduvanzaTask/static/js/2.277985d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b43e7739f0dd4ce7d1",
+    "url": "/halahalliaravind/EduvanzaTask/static/js/3.dbbe5861.chunk.js"
+  },
+  {
+    "revision": "2dcd94077163ba688835",
+    "url": "/halahalliaravind/EduvanzaTask/static/js/4.04c72cf7.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/halahalliaravind/EduvanzaTask/static/js/4.04c72cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236312e0e569dfc7f277",
+    "url": "/halahalliaravind/EduvanzaTask/static/js/5.67d04985.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/halahalliaravind/EduvanzaTask/static/js/5.67d04985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "835c5b7508f740b4ac29",
+    "url": "/halahalliaravind/EduvanzaTask/static/js/main.d73f42ae.chunk.js"
+  },
+  {
+    "revision": "20e5b5bf25fec986c0df",
+    "url": "/halahalliaravind/EduvanzaTask/static/js/runtime-main.2667501e.js"
+  }
+]);
